e9925dc48cbcb1eefbe57a7cd5143e29b1162bc7
Samuel Bello <samuelbellocontato@gmail.com>
2023-09-19 13:08:09 -0300

    Concatenar vários elementos

---
+++ src/ConcatenarElementos.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class ConcatenarElementos {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		String nome = "Maria";
+		int idade = 30;
+		double salario = 4000.0;
+		
+		System.out.printf("%s tem %d anos e recebe R$ %.2f.%n", nome, idade, salario);
+	}
+}
--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,5 +1,6 @@
 public class OlaMundo{
     public  static void main (String[] args){
         System.out.println("Olá, mundo!");
+        System.out.println("Bom dia!");
     }
 }
