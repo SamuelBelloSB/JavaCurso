8590818da187741450e15165db6ebb53cd3cbda9
Smauel Bello <samuelbellocontato@gmail.com>
2024-04-10 14:53:13 -0300

    exercicio entrada de dados 02 usando scanner para mais de uma entrada de dados na mesma linha separado por espaço

---
+++ src/EntradaDeDados02.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaDeDados02 {
+
+	public static void main(String[] args) {
+		System.out.println("Digite o seu nome, idade e peso separado por espaço: ");
+		Scanner sc = new Scanner(System.in);
+		String nome;
+		int idade;
+		Locale.setDefault(Locale.US);
+		double peso;
+		
+		// Para inserir mais de um elemento na entrada de dados separado por virgula
+		// usamos .nextLine();
+		nome = sc.next();
+		idade = sc.nextInt();
+		peso = sc.nextDouble();
+		
+
+		System.out.printf("Nome: %s%nIdade: %d anos%nPeso: %.1fKg", nome, idade, peso);
+	}
+}
