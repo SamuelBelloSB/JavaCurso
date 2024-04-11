b144d1f6d0a3ea24f3dc2fdd9a9709ad418aca27
Smauel Bello <samuelbellocontato@gmail.com>
2024-04-11 19:27:17 -0300

    exercicio de fixacão 10 - condicional multiplo ou não multiplo

---
+++ src/ExercicioFixacao10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ExercicioFixacao10 {
+
+	public static void main(String[] args) {
+		/*
+		 * Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem
+		 * "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são
+		 * múltiplos entre si. Atenção: os números devem poder ser digitados em ordem
+		 * crescente ou decrescente.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		if (A % B == 0 || B % A == 0) System.out.println("São multíplos");
+		else System.out.println("Não são multíplos");
+		
+		sc.close();
+	}
+}
